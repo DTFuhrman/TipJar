c8e7ea53cb4a415eaa913396c31e4e13639b55b1
David Fuhrman <DTFuhrman@gmail.com>
2020-03-26 19:46:04 -0400

    made a comment. fixed a param.

--- tipjar/src/main/webapp/js/header.js
+++ tipjar/src/main/webapp/js/header.js
@@ -6,12 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
 	
 	const menuButton = document.getElementById("menu-burger");
 	const menuFull = document.getElementById('navbar');
-	menuButton.addEventListener('click', (menuFull ,menuButton) => {
+	menuButton.addEventListener('click', () => {
 		toggleMenu(menuFull ,menuButton);
 	  })
 })
 
-
+//comment
 function toggleMenu(menu, burger) {
 	menu.classList.toggle('is-active');
 	burger.classList.toggle('is-active');
